b111c6e5281f804eb5c7222a47f964c9e296045c
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-02-27 17:49:34 +0300

    up

---
+++ Lab Material/Lab 5.2 Revision/js/index.js
@@ -0,0 +1,25 @@
+// const num = [30, 54, 58, 6, 7, -10]
+// const [first, second, ...other] = num
+
+// const person = {
+//     name: 'Mohamed',
+//     age: 25,
+//     job: 'Web Developer',
+//     gender: 'Male',
+//     hobbies: ['Football', 'Movies', 'Basketball']
+// }
+// console.log(num);
+// console.log(`First is ${first}`);
+// console.log(`Second is ${second}`);
+// console.log(`Other is ${other}`);
+
+// const { name, age, job, gender, hobbies } = person
+// // display mohamed info
+// console.log(`Name ${person.name}`);
+// console.log(`Age ${person.age}`);  
+
+
+const num = [1, 4, 10, 111, 2, 21, 222, 7, 4, 9, -1, 4, -2]
+console.log(num.sort((a, b) => a - b));
+
+

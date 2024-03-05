501e253242cb1a39b36f7f58d78502ef371df847
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-05 15:08:22 +0300

    Update bank.js

--- Lab Material/Lab 6- JS OOP/Tutorial/PartB-Testing/repo/bank.js
+++ Lab Material/Lab 6- JS OOP/Tutorial/PartB-Testing/repo/bank.js
@@ -3,7 +3,23 @@ const accounts = [
         accountNo: 123,
         balance: 500,
         type: "saving"
+    },
+    {
+        accountNo: 234,
+        balance: 4000,
+        type: "current"
+    },
+    {
+        accountNo: 345,
+        balance: 35000,
+        type: "current"
+    },
+    {
+        accountNo: 456,
+        balance: 60000,
+        type: 'saving'
     }
+
 ]
 export function convertFromUSToQAR(amount) {
     return amount * 3.64;
@@ -15,4 +31,8 @@ export function addAccount(account) {
 
 export function getAccounts() {
     return accounts
+}
+
+export function deposit(accountNo, amount) {
+
 }

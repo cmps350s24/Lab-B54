9d1c33ca38bd39dc0fe3652876906a0e3e5a8b7a
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-02 14:16:18 +0300

    done

---
+++ Lab Material/Lab 9– Web Pages using Next.js and React/lab08-bank-app/app/page.js
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default async function Home() {
+
+  // we will get the data
+  // const accounts = 
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}

a975a8fc16a04f82e43387cdd0681b42fe7a2a84
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-16 15:01:45 +0300

    form

--- Lab Material/Lab 10– Web Pages using Next.js and React-Formss/Base Code/lab10-bank-app/app/accounts/upsert/page.jsx
+++ Lab Material/Lab 10– Web Pages using Next.js and React-Formss/Base Code/lab10-bank-app/app/accounts/upsert/page.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
+import styles from '@/app/page.module.css'
 
 export default function Add() {
     return (
         <>
             <h3>Add Account</h3>
-            <form id="account-form">
-                <label for="firstname">First Name</label>
+            <form id="account-form" className={styles.form}>
+                <label htmlFor="firstname">First Name</label>
                 <input type="text" name="firstname" id="firstname" />
 
-                <label for="lastname">Last Name</label>
+                <label htmlFor="lastname">Last Name</label>
                 <input type="text" name="lastname" id="lastname" />
 
-                <label for="acctType">Account Type</label>
+                <label htmlFor="acctType">Account Type</label>
                 <select name="acctType" id="acctType" required>
                     <option value=""></option>
                     <option value="Saving">Saving</option>
                     <option value="Current">Current</option>
                 </select>
 
-                <label for="balance">Balance</label>
+                <label htmlFor="balance">Balance</label>
                 <input type="number" name="balance" id="balance" required />
 
-                <label for="email">Email</label>
+                <label htmlFor="email">Email</label>
                 <input type="email" name="email" id="email" />
 
-                <label for="dateOpened">Date Opened</label>
+                <label htmlFor="dateOpened">Date Opened</label>
                 <input type="date" name="dateOpened" id="dateOpened" />
 
-                <label for="gender">Gender</label>
+                <label htmlFor="gender">Gender</label>
                 <select name="gender" id="gender" required>
                     <option value=""></option>
                     <option value="Male">Male</option>
@@ -35,7 +36,7 @@ export default function Add() {
                     <option value="Other">Other</option>
                 </select>
 
-                <label for="profileImage">Profile Image URL</label>
+                <label htmlFor="profileImage">Profile Image URL</label>
                 <input type="url" name="profileImage" id="profileImage" />
 
                 <button type="Submit">Submit</button>

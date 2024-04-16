4bf92790fea2db8c7ff0e96e180844f121f67229
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-16 14:57:40 +0300

    updated

--- Lab Material/Lab 10– Web Pages using Next.js and React-Formss/Base Code/lab10-bank-app/app/accounts/add/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Add() {
-    return (
-        <div><h1>Add Page</h1></div>
-    )
-}
--- Lab Material/Lab 10– Web Pages using Next.js and React-Formss/Base Code/lab10-bank-app/app/accounts/edit/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Edit() {
-    return (
-        <div><h1>Edit Page</h1></div>
-    )
-}
